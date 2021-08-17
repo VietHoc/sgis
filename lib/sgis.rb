51d1a13372f8c8c0196a332ffb99096af473a45b
Hoc Le <viethocle2603@gmail.com>
2021-08-17 08:54:36 +0700

    Add test user class

--- lib/sgis.rb
+++ lib/sgis.rb
@@ -4,6 +4,29 @@ require_relative "sgis/version"
 
 module Sgis
   class Error < StandardError; end
-  class Course
+  class UserGem
+    def initialize
+      @context = Api::V1::ReceiptsController.new
+    end
+
+    def last_user
+      User.last
+    end
+
+    def ios_devices
+      @context.ios_devices?
+    end
+
+    def ios
+      @context.ios
+    end
+
+    def esm_current_user
+      @context.current_user
+    end
+
+    def user
+      @current_user ||= warden.authenticate(scope: :user)
+    end
   end
 end
